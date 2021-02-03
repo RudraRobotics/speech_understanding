907a69a09212f73405a0c255d5b3ae439081c8d8
Dinesh Lama <dinesh99981@gmail.com>
2021-02-03 17:17:13 +0545

    latest changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # speech_understanding
+
+This is the speech recognition ros module  
+Features:  
+1. Text to speech conversion with picovoice cheetah  
+2. rasa-nlu and rasa-core for NLP features    
+3. text to speech conversion   
+4. ros nav_msgs/Goal [] publisher as a mission with voice commands  
+5. Offiline speech recongition features
+6. Supports multiple plateform like embedded, desktop etc  
+7. Mission cancelation feature   
+8. Mission changing feature  
+
+Dependencies:  
+1. numpy  
+2. ROS noetic  
+
+How to run:  
+1. roslaunch speech_understanding speech_understanding.launch  
+
+After this a user can speak and give multiple commands and talk with the robot, offline. 
--- scripts/speech_understanding_node.py
+++ scripts/speech_understanding_node.py
@@ -1,37 +1,46 @@
-#
-#    Copyright 2018 Picovoice Inc.
-#
-#    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
-#    file accompanying this source.
-#
-#    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-#    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-#    specific language governing permissions and limitations under the License.
-#
-
-import os
+#!/usr/bin/env python3
+import rospy
 import sys
+from audio_common_msgs.msg import AudioData
+import os
+import numpy as np
 
-import soundfile
+sys.path.append(os.path.join(os.path.dirname(__file__), '../binding/python'))
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../binding/python'))
+from cheetah import Cheetah
 
-from leopard import Leopard
+class SpeechUnderstanding:
 
+  def __init__(self):
+    self.library_path = rospy.get_param("/speech_understanding/library_path")
+    self.acoustic_model_path = rospy.get_param("/speech_understanding/acoustic_model_path")
+    self.language_model_path = rospy.get_param("/speech_understanding/language_model_path")
+    self.license_path = rospy.get_param("/speech_understanding/license_path")
+
+    self.cheetah = Cheetah(
+        library_path=self.library_path,
+        acoustic_model_path=self.acoustic_model_path,
+        language_model_path=self.language_model_path,
+        license_path=self.license_path,
+        endpoint_duration_sec=1)
+
+    self.sub = rospy.Subscriber("audio", AudioData, self.callback)
+
+  def callback(self,data):
+    try:
+        numpydata = np.frombuffer(data.data, dtype=np.int16)
+        audio_tuple_data = tuple(numpydata)
+        print(len(audio_tuple_data))
+        partial_transcript, is_endpoint = self.cheetah.process(audio_tuple_data)
+        if is_endpoint:
+            print(self.cheetah.flush())
+    except Exception as e:
+        pass #print(e)
 
 if __name__ == '__main__':
-
-    leopard = Leopard(
-        library_path='/home/ubuntu/Downloads/leopard/lib/linux/x86_64/libpv_leopard.so',
-        acoustic_model_path='/home/ubuntu/Downloads/leopard/lib/common/acoustic_model.pv',
-        language_model_path='/home/ubuntu/Downloads/leopard/lib/common/language_model.pv',
-        license_path='/home/ubuntu/Downloads/leopard/resources/license/license_leo_v1.0.0_linux_expires_3_4_2021.lic')
-
-    audio_path='/home/ubuntu/Downloads/leopard/resources/audio_samples/test.wav'
-    audio, sample_rate = soundfile.read(audio_path, dtype='int16')
-    if sample_rate != leopard.sample_rate:
-        raise ValueError('Leopard can only process audio data with sample rate of %d' % leopard.sample_rate)
-    transcript = leopard.process(audio)
-
-    print(transcript)
-
+  rospy.init_node('speech_understanding', anonymous=True)
+  speechUnderStanding = SpeechUnderstanding()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
